2767eef76eb1ba135e0df67bf70a98c7b555e30e
Anshini Chaturvedi <anshinichaturvedi888@gmail.com>
2024-11-28 22:04:44 +0530

    function_q19.py

---
+++ coding-assignment/function/function_q19.py
@@ -0,0 +1,5 @@
+19. Write a function to generate a list of squares of numbers from 1 to n.
+def list_of_squares(n):
+    return [i ** 2 for i in range(1, n + 1)]
+
+print(list_of_squares(5))  # Output: [1, 4, 9, 16, 25]
