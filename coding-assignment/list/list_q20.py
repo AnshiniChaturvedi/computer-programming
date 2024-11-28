62665411434de0ba6be222d19968588e4c65a21c
Anshini Chaturvedi <anshinichaturvedi888@gmail.com>
2024-11-28 21:18:18 +0530

    list_q20.py

---
+++ coding-assignment/list/list_q20.py
@@ -0,0 +1,6 @@
+20. Find common elements in two lists
+Question: Find common elements between [1, 2, 3] and [2, 3, 4].
+lst1 = [1, 2, 3]
+lst2 = [2, 3, 4]
+common_elements = list(set(lst1) & set(lst2))
+print(common_elements) 
